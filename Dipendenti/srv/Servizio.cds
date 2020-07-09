95aeef68220220c519496eee5b7b58890ee237f5
Roberto Pini <r.pini@neua.it>
2020-07-09 15:16:06 +0000

    2nd

--- Dipendenti/srv/Servizio.cds
+++ Dipendenti/srv/Servizio.cds
@@ -3,7 +3,7 @@ using { Dipartimenti as DipaModel, Dipendenti as DipeModel } from '../db/schema'
 service DipService {
   entity Dipendenti as select from DipeModel {
       *,
-      null as longevita: Integer
+      null as anzianita: Integer
   };
   
   entity Dipartimenti as projection on DipaModel;
--- Dipendenti/srv/Servizio.js
+++ Dipendenti/srv/Servizio.js
@@ -1,15 +1,21 @@
 const cds = require('@sap/cds')
+const moment = require('moment');
+
 module.exports = async function ( )
 {
 // registriamo l'handler che scatterà alla chiamata alla funzione  
 await cds.connect()
 const { Dipendenti } = cds.entities 
 
+// Funzione calcolo salario medio
 this.on('avgSalary', calcAvg );
 
-this.on('READ','Dipendenti')
-
-
+// Campo calcolato - anzianità
+this.after ('READ','Dipendenti', each => {
+    let now = moment()
+    let b = moment(each.dataAss)
+    each.anzianita = now.diff(b,'years')
+})
 
 
 
